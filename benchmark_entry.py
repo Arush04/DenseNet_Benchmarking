63c3623ac05dba6ef135f2000b64d1e7ae26a1f3
Arush04 <sharmaarush591@gmail.com>
2025-09-14 23:35:27 +0530

    download true

--- benchmark_entry.py
+++ benchmark_entry.py
@@ -67,8 +67,8 @@ def make_dataloaders(batch_size: int, num_workers: int = 2, max_eval_batches: in
         T.ToTensor(),
         T.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5)),
     ])
-    train_set = torchvision.datasets.CIFAR10(root='./data', train=True, download=False, transform=transform)
-    val_set = torchvision.datasets.CIFAR10(root='./data', train=False, download=False, transform=transform)
+    train_set = torchvision.datasets.CIFAR10(root='./data', train=True, download=True, transform=transform)
+    val_set = torchvision.datasets.CIFAR10(root='./data', train=False, download=True, transform=transform)
 
     train_loader = DataLoader(train_set, batch_size=batch_size, shuffle=True, num_workers=num_workers, pin_memory=True)
     val_loader = DataLoader(val_set, batch_size=batch_size, shuffle=False, num_workers=num_workers, pin_memory=True)
